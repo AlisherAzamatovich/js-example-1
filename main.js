257a442d60b0fbfed377a96b3be30ae70b0b4f82
@alisher <alisherazamatovich027@gmail.com>
2023-08-03 21:36:31 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+var oldYear = +prompt("Введите возраст");
+
+// if ( oldYear > 0 && oldYear <= 18) {
+//   alert("Вы еще молоды, Вам нужно учиться")
+// } else if (oldYear > 18 && oldYear == 50) {
+//   alert("Вам нужно работать")
+// } else if (50 < oldYear == 59) {
+//   alert("Вам скоро на пенсию")
+// } else if (59 < oldYear == 100) {
+//   alert("Вы пенсионер")
+// } else {
+//   alert("Что то пошло не так!!")
+// }
