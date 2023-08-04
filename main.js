64a2165a2e40a899a6bd40a088dca37c2f9ddf7f
@alisher <alisherazamatovich027@gmail.com>
2023-08-04 09:37:45 +0500

    second-commit

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-var oldYear = +prompt("Введите возраст");
+var oldYear = +prompt("Введите ваш возраст: ")
 
-// if ( oldYear > 0 && oldYear <= 18) {
-//   alert("Вы еще молоды, Вам нужно учиться")
-// } else if (oldYear > 18 && oldYear == 50) {
-//   alert("Вам нужно работать")
-// } else if (50 < oldYear == 59) {
-//   alert("Вам скоро на пенсию")
-// } else if (59 < oldYear == 100) {
-//   alert("Вы пенсионер")
-// } else {
-//   alert("Что то пошло не так!!")
-// }
+if (0 < oldYear && oldYear <= 100) {
+  if ( oldYear <= 18 ) {
+    alert("Вы еще молоды, Вам нужно учиться")
+  } else if ( oldYear <= 50 ) {
+    alert("Вам нужно работать")
+  } else if (oldYear <= 59) {
+    alert("Вам скоро на пенсию")
+  } else if (oldYear <= 100) {
+    alert("Вы пенсионер")
+  }
+} else {
+  alert("Что то пошло не так!!")
+}
